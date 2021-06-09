901af40f7503cfe5cc2315635b4cbfb7ef6aaf8f
jakefroeb <jake.herman.froeb@gmail.com>
2021-06-09 09:19:56 -0500

    set up the initial models for the restapi

---
+++ petsapi/models/pet.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Pet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    animal_type = models.models.ForeignKey("Animal_Type", on_delete=models.CASCADE)
---
+++ petsapi/models/pet_action.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Pet_Action(models.Model):
+    pet = models.ForeignKey("Pet", on_delete=models.CASCADE)
+    action = models.ForeignKey("Action", on_delete=models.CASCADE)
+    action_date = models.models.DateTimeField(auto_now_add=True)
