5ee7fac67c98625d52f42ed1dfb24c0880f36f2d
jakefroeb <jake.herman.froeb@gmail.com>
2021-06-10 16:50:55 -0500

    delete functionality

--- petsapi/views/pet_view.py
+++ petsapi/views/pet_view.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from petsapi.models import Pet, Animal_Type
 from django.contrib.auth.models import User
+from rest_framework import status
 
 
 class PetView(ViewSet):
@@ -28,7 +29,15 @@ class PetView(ViewSet):
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
-
+    def destroy(self, request, pk=None):
+        try:
+            pet = Pet.objects.get(pk=pk)
+            pet.delete()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Pet.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     def list(self, request):
         user = request.auth.user
         pets = Pet.objects.filter(user=user)
